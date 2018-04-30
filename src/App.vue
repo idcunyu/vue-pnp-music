df3ba6bd0cba6f782127fb35eb50420600f34385
vernonn <695035427@qq.com>
2018-04-30 23:59:28 +0800

    initialise

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    hello world
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped></style>
