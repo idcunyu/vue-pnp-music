207363c8feed4664f5e92ea5b7a5ca9d3d5624f9
vernonn <695035427@qq.com>
2018-05-03 22:22:43 +0800

    添加主页头部

---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="m-header">
+		<div class="icon"></div>
+		<h1 class="text">Pineapple Music</h1>
+	</div>
+</template>
+
+<script type="text/ecmascript">
+	export default {
+		name: "MHeader"
+	}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+	
+</style>
